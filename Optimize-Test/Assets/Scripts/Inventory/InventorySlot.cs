e3410f4b868ac12ad8721bd1636959ff6bc9bcd8
PauloVianna <paulovianna.lwk@gmail.com>
2022-02-12 18:50:08 -0300

    InventorySlot script and prefab

---
+++ Optimize-Test/Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image Background;
+    public Image Icon;
+    public TextMeshProUGUI Name;
+    public Button Button;
+}
